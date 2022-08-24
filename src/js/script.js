f69df0e76d3201d2b855b11cff2b2ec4a3ae6e19
cybertcode <cybertcode@gmail.com>
2022-08-23 19:35:25 -0500

    Testimonio y galería
    cool
    
    Co-Authored-By: MKEVYN <40509580+cybert22@users.noreply.github.com>

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,35 @@ open.addEventListener('click', function () {
 close.addEventListener('click', function () {
 	nav.classList.remove('right-0');
 });
+
+//Inicio ABRIR lightbox
+const lightbox = document.querySelector('.lightbox');
+const lightboxBody = document.querySelector('.lightbox-body');
+const images = document.querySelectorAll('#gallery');
+images.forEach(function (image) {
+	// console.log(image);
+	image.addEventListener('click', function () {
+		lightbox.classList.add('scale-100');
+		const img = document.createElement('img');
+		img.src = image.src;
+		// console.log(img)
+		img.classList.add('w-full');
+		img.classList.add('h-full');
+		img.classList.add('object-cover');
+		if (lightboxBody.children[0]) {
+			lightboxBody.removeChild(lightboxBody.children[0]);
+		}
+		lightboxBody.appendChild(img);
+		// console.log(lightboxBody)
+	});
+});
+//FIN ABRIR lightbox
+//INICIO  CERRAR lightbox
+lightbox.addEventListener('click', function (e) {
+	const idName = e.target.getAttribute('id')
+	if (idName === 'lightbox') {
+		lightbox.classList.remove('scale-100');
+	}
+});
+//FIN  CERRAR lightbox
+
